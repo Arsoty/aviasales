0e1e343d9911559b04bd4435e8571e53cba9a032
Artur Sokolov <artur.sokolov@smissltd.com>
2022-02-14 16:05:34 +0200

    filtration changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { Provider } from 'react-redux'
 // import logo from './logo.svg';
 import './App.css'
 import { store } from './store'
-import TicketList from './components/TicketList'
+// import TicketList from './components/TicketList'
+import { TicketFilter } from './components/TicketFilter'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function App() {
     return (
         <Provider store={store}>
             <div className="App">
-                <TicketList />
+                <TicketFilter />
             </div>
         </Provider>
     )
